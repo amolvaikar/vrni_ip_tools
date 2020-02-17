54d71ad60c4af268b453d67c81461b4abc18bd7a
avaikar <avaikar@vmware.com>
2020-02-17 19:20:54 +0530

    Added free IP address finder tool

---
+++ FreeIPFinder/free_ip_finder_tool.py
@@ -0,0 +1,92 @@
+
+
+import sys
+import requests
+import json
+from optparse import OptionParser
+import ipaddress
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def get_existing_ips(url, user_id, password, subnet):
+	existing_ip_set = set()
+	with requests.Session() as session:
+		session.auth = (user_id, password)
+
+		data = '{"username":"' + user_id + '","password":"' + password + '","tenantName":"' + sys.argv[1] +\
+			   '","vIDMURL":"","redirectURL":"","authenticationDomains":{"0":{"domainType":"LOCAL_DOMAIN","domain":"localdomain","redirectUrl":""}},'+\
+			   '"currentDomain":0,"domain":"localdomain","nDomains":1,"serverTimestamp":false,"loginFieldPlaceHolder":"Username"}'
+
+		# Instead of requests.get(), you'll use session.get()
+		response = session.post(url+"/api/auth/login", data=data, verify=False, headers={'content-type':'application/json', 'accept':'application/json'})
+		#print response
+		loaded_json = json.loads(response.content)
+		session.headers["x-vrni-csrf-token"] = loaded_json["csrfToken"]
+
+		start_idx = 0
+		ask_length = 50
+		ip_search_query = "/api/search/query?searchString=IP%20Endpoint%20where%20IP%20Address%20like%20" + str(subnet) + \
+						  "&includeObjects=false&includeFacets=true&includeMetrics=false&includeEvents=false&startIndex=" + str(start_idx) +\
+						  "&maxItemCount=" + str(ask_length) + "&dateTimeZone=%2B05%3A30&sourceString=USER&includeModelKeyOnly=false"
+		response = session.get(url+ip_search_query, verify=False)
+		#print response
+		found_ips = json.loads(response.content)
+		while len(found_ips['resultList']) > 0:
+			for result in found_ips['resultList']:
+				existing_ip_set.add(result['searchContext']['name'])
+			current_len = len(found_ips['resultList'])
+			start_idx += current_len
+			ip_search_query = "/api/search/query?searchString=IP%20Endpoint%20where%20IP%20Address%20like%20" + str(subnet) + \
+							  "&includeObjects=false&includeFacets=true&includeMetrics=false&includeEvents=false&startIndex=" + str(start_idx) + \
+							  "&maxItemCount=" + str(ask_length) + "&dateTimeZone=%2B05%3A30&sourceString=USER&includeModelKeyOnly=false"
+			response = session.get(url + ip_search_query, verify=False)
+			found_ips = json.loads(response.content)
+	return existing_ip_set
+
+if __name__ == '__main__':
+
+	parser = OptionParser()
+	parser.add_option("-d", "--destination", dest="server",
+					  help="vRNI Server IP/fqdn")
+
+	parser.add_option("-u", "--user",
+					  dest="uid",
+					  help="vRNI User")
+
+	parser.add_option("-p", "--password",
+					  dest="password",
+					  help="vRNI User's password")
+
+	parser.add_option("-s", "--subnet",
+					  dest="subnet",
+					  help="subnet to search in. e.g.: 192.168.21.0/24")
+
+	(options, args) = parser.parse_args()
+
+	if options.server is None or options.uid is None or options.password is None or options.subnet is None:
+		parser.print_help()
+		print "Insufficient arguments"
+		sys.exit(1)
+	
+	url = "https://" + options.server
+	ip_argument = options.subnet
+	user_id = options.uid
+	password = options.password
+	subnet = options.subnet
+
+	arg_subnet = subnet.split("/")[0]
+	arg_subnet = arg_subnet.replace(".0", ".*")
+	existing_ip_set = get_existing_ips(url, user_id, password, arg_subnet)
+
+	theoretical_ip_set = ipaddress.IPv4Network(unicode(subnet, "utf-8"))
+
+	# print existing_ip_set
+	# print theoretical_ip_set
+
+	print "Free IP addresses in given subnet:"
+	for tip in theoretical_ip_set:
+		if not tip.compressed in existing_ip_set:
+			print tip
+
+
+
