80189b7655f0eed1957a9074ebe38c2689977dc9
avaikar <avaikar@vmware.com>
2021-02-04 02:23:10 +0530

    script to send summary email

--- EmailSummarySender/send_summary_email.py
+++ EmailSummarySender/send_summary_email.py
@@ -1,5 +1,4 @@
-
-
+from datetime import datetime
 import sys
 import requests
 import json
@@ -10,14 +9,10 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import time
 import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 sender = 'noreply@vmware.com'
-
-message = """From: vRNI Summary Ninja <noreply@vmware.com>
-To: vRNI Master <to@todomain.com>
-Subject: vRNI Daily Summary
-
-"""
 
 def open_vrni_session(url, user_id, password):
 	try:
@@ -58,69 +53,56 @@ def open_vrni_session(url, user_id, password):
 def current_milli_time():
     return int(round(time.time() * 1000))
 
-def send_summary_email(session, target_email):
+def send_summary_email(session, options):
 	try:
-		#Information to send:
-		#Total traffic in last 24 hours, compared with previous day and same day last week
-		#&timestamp=1612265665538&timeRangeString=+between+timestamp+1612178885460+and+timestamp+1612265285460
-		current_time_stamp = current_milli_time()
-		one_day_millisecond = 24 * 60 * 60 * 1000
-		yesterday_time_stamp = current_time_stamp - one_day_millisecond
-		day_before_yesterday_timestamp = yesterday_time_stamp - one_day_millisecond
-		same_day_last_week_timestamp_start = current_time_stamp - (7 * one_day_millisecond)
-		same_day_last_week_timestamp_end = current_time_stamp - (6 * one_day_millisecond)
+		email_message = get_traffic_summary(session)
+		email_message += get_problem_summary(session)
 
-		rx_bytes_query = urllib.quote_plus("sum(total rx bytes) of switch ports")
+		if options.email_server is None:
+			options.email_server = "email-smtp.us-west-2.amazonaws.com"
 
-		#Find todays total Rx
-		todays_time_range_string = "&timeRangeString= between timestamp " + str(
-			yesterday_time_stamp) + " and timestamp " + str(current_time_stamp)
-		todays_rx_bytes_query_request = "/api/search/query?searchString=" + rx_bytes_query + todays_time_range_string
-		response = session.get(url + todays_rx_bytes_query_request, verify=False)
-		found_metrics = json.loads(response.content)
-		#todo: handle error
-		todays_rx_bytes = int(found_metrics['aggregates2'][0]['aggs']['SUM'])
+		if options.email_server_login is None:
+			options.email_server_login = "AKIAJCQMNKULV43XLG6A"
 
-		#Find yesterday's total Rx
-		yesterdays_time_range_string = "&timeRangeString= between timestamp " + str(
-			day_before_yesterday_timestamp) + " and timestamp " + str(yesterday_time_stamp)
-		yesterdays_rx_bytes_query_request = "/api/search/query?searchString=" + rx_bytes_query + yesterdays_time_range_string
-		response = session.get(url + yesterdays_rx_bytes_query_request, verify=False)
-		found_metrics = json.loads(response.content)
-		#todo: handle error
-		yesterdays_rx_bytes = int(found_metrics['aggregates2'][0]['aggs']['SUM'])
+		if options.email_server_password is None:
+			options.email_server_password = "Ajg+/jfsHwCL9W+GBmhrYII1/blbLyJxbFiKPl/wDgOG"
 
-		#Find todays total tx
-		tx_bytes_query = urllib.quote_plus("sum(total tx bytes) of switch ports")
-		todays_tx_bytes_query_request = "/api/search/query?searchString=" + tx_bytes_query + todays_time_range_string
-		response = session.get(url + todays_tx_bytes_query_request, verify=False)
-		found_metrics = json.loads(response.content)
-		#todo: handle error
-		todays_tx_bytes = int(found_metrics['aggregates2'][0]['aggs']['SUM'])
+		if options.target_email is None:
+			options.target_email = "avaikar@vmware.com"
 
-		#Find yesterday's total tx
-		yesterdays_tx_bytes_query_request = "/api/search/query?searchString=" + tx_bytes_query + yesterdays_time_range_string
-		response = session.get(url + yesterdays_tx_bytes_query_request, verify=False)
-		found_metrics = json.loads(response.content)
-		#todo: handle error
-		yesterdays_tx_bytes = int(found_metrics['aggregates2'][0]['aggs']['SUM'])
-
-		total_today =  todays_rx_bytes + todays_tx_bytes
-		total_yest = yesterdays_rx_bytes + yesterdays_tx_bytes
-
-		email_message = "Total Network Traffic (Read + Write) for today is: " + str((total_today)/(1024*1024*1024)) + " GB\n"
-		if (total_today > total_yest):
-			email_message += "Which is "  + str(total_today - total_yest) + " bytes more than yesterday's traffic\n\n"
-		else:
-			email_message += "Which is " + str(total_yest - total_today) + " bytes less than yesterday's traffic\n\n"
-
-		email_message += "To find more details about traffic patterns and problems, log on to " + url + "\n\nWith Best Regards,\nYour Friendly Neighborhood vRNI\n"
-
-		smtpObj = smtplib.SMTP('email-smtp.us-west-2.amazonaws.com', 587)
+		smtpObj = smtplib.SMTP(options.email_server, 587)
 		smtpObj.starttls()
-		smtpObj.login("AKIAJCQMNKULV43XLG6A", "Ajg+/jfsHwCL9W+GBmhrYII1/blbLyJxbFiKPl/wDgOG")
-		receivers = ['praving@vmware.com', 'avaikar@vmware.com']
-		smtpObj.sendmail(sender, receivers, message + email_message)
+		smtpObj.login(options.email_server_login, options.email_server_password)
+		receivers = [options.target_email]
+		html = """\
+		<html>
+		  <head></head>
+		  <body>
+			<p>Hi Admin!</p>
+
+			<p>This is your network summary for&nbsp;"""
+		now = datetime.now()
+		date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+		html += date_time + "</p>"
+
+		html_footer = """<p>With Best Regards,</p><p>Your Friendly Neighborhood vRNI</p>
+						  	</body>
+						</html>
+					  """
+
+		# Create message container - the correct MIME type is multipart/alternative.
+		msg = MIMEMultipart('alternative')
+		msg['Subject'] = "vRNI Network Summary"
+		msg['From'] = sender
+		msg['To'] = options.target_email
+
+		#part1 = MIMEText(message + email_message, 'plain')
+		part2 = MIMEText(html + email_message + html_footer, 'html')
+
+		#msg.attach(part1)
+		msg.attach(part2)
+
+		smtpObj.sendmail(sender, receivers, msg.as_string())
 	#Total open problems in last 24 hours (up by or down by nn over previous day)
 
 	except requests.exceptions.ConnectionError as connection_exception:
@@ -128,6 +110,101 @@ def send_summary_email(session, target_email):
 		print connection_exception.message
 		return None
 	return ""
+
+current_time_stamp = current_milli_time()
+one_day_millisecond = 24 * 60 * 60 * 1000
+yesterday_time_stamp = current_time_stamp - one_day_millisecond
+day_before_yesterday_timestamp = yesterday_time_stamp - one_day_millisecond
+same_day_last_week_timestamp_start = current_time_stamp - (7 * one_day_millisecond)
+same_day_last_week_timestamp_end = current_time_stamp - (6 * one_day_millisecond)
+
+def get_problem_summary(session):
+	#Information to send:
+	# Total open problems in last 24 hours
+	open_problems_query = urllib.quote_plus("open problems")
+	# Find todays total Rx
+	todays_time_range_string = "&timeRangeString= between timestamp " + str(
+		yesterday_time_stamp) + " and timestamp " + str(current_time_stamp)
+	todays_open_problems_query_request = "/api/search/query?searchString=" + open_problems_query + todays_time_range_string
+	response = session.get(url + todays_open_problems_query_request, verify=False)
+	found_metrics = json.loads(response.content)
+	# todo: handle error
+	todays_open_problems = int(found_metrics['count'])
+
+	# Find yesterday's open problems
+	yesterdays_time_range_string = "&timeRangeString= between timestamp " + str(
+		day_before_yesterday_timestamp) + " and timestamp " + str(yesterday_time_stamp)
+	yesterdays_open_problems_query_request = "/api/search/query?searchString=" + open_problems_query + yesterdays_time_range_string
+	response = session.get(url + yesterdays_open_problems_query_request, verify=False)
+	found_metrics = json.loads(response.content)
+	# todo: handle error
+	yesterdays_open_problems = int(found_metrics['count'])
+
+	email_message = """<p><u><strong>Open Problems</strong></u></p>
+
+						<p>&nbsp; &nbsp; Today&#39;s open problem count: <span style="color:#ff8c00"><u><strong>"""
+	email_message += str(todays_open_problems)
+	email_message += """</strong></u></span></p>
+
+						<p>&nbsp; &nbsp; This is """
+	if todays_open_problems > yesterdays_open_problems:
+		email_message += str(todays_open_problems - yesterdays_open_problems) + '<u><strong><span style="color:#ff0000"> more</span></strong></u> than yesterday</p>'
+	else:
+		email_message += str(yesterdays_open_problems - todays_open_problems) + '<u><strong><span style="color:#008000"> less</span></strong></u> than yesterday</p>'
+	email_message += '<p>&nbsp; &nbsp; To see a list of all open problems, click &nbsp;<a href="'\
+					 + url + '/#search/query/%22open%20problems%22/timemeta/{%22timePreset%22%3A%22Last%2024%20hours%22}/sourceString/%22USER%22">'
+	email_message += 'here</a></p>'
+	return email_message
+
+def get_traffic_summary(session):
+	# Information to send:
+	# Total traffic in last 24 hours, compared with previous day and same day last week
+	# &timestamp=1612265665538&timeRangeString=+between+timestamp+1612178885460+and+timestamp+1612265285460
+
+	rx_bytes_query = urllib.quote_plus("sum(total rx bytes) of switch ports")
+	# Find todays total Rx
+	todays_time_range_string = "&timeRangeString= between timestamp " + str(
+		yesterday_time_stamp) + " and timestamp " + str(current_time_stamp)
+	todays_rx_bytes_query_request = "/api/search/query?searchString=" + rx_bytes_query + todays_time_range_string
+	response = session.get(url + todays_rx_bytes_query_request, verify=False)
+	found_metrics = json.loads(response.content)
+	# todo: handle error
+	todays_rx_bytes = int(found_metrics['aggregates2'][0]['aggs']['SUM'])
+	# Find yesterday's total Rx
+	yesterdays_time_range_string = "&timeRangeString= between timestamp " + str(
+		day_before_yesterday_timestamp) + " and timestamp " + str(yesterday_time_stamp)
+	yesterdays_rx_bytes_query_request = "/api/search/query?searchString=" + rx_bytes_query + yesterdays_time_range_string
+	response = session.get(url + yesterdays_rx_bytes_query_request, verify=False)
+	found_metrics = json.loads(response.content)
+	# todo: handle error
+	yesterdays_rx_bytes = int(found_metrics['aggregates2'][0]['aggs']['SUM'])
+	# Find todays total tx
+	tx_bytes_query = urllib.quote_plus("sum(total tx bytes) of switch ports")
+	todays_tx_bytes_query_request = "/api/search/query?searchString=" + tx_bytes_query + todays_time_range_string
+	response = session.get(url + todays_tx_bytes_query_request, verify=False)
+	found_metrics = json.loads(response.content)
+	# todo: handle error
+	todays_tx_bytes = int(found_metrics['aggregates2'][0]['aggs']['SUM'])
+	# Find yesterday's total tx
+	yesterdays_tx_bytes_query_request = "/api/search/query?searchString=" + tx_bytes_query + yesterdays_time_range_string
+	response = session.get(url + yesterdays_tx_bytes_query_request, verify=False)
+	found_metrics = json.loads(response.content)
+	# todo: handle error
+	yesterdays_tx_bytes = int(found_metrics['aggregates2'][0]['aggs']['SUM'])
+	total_today = todays_rx_bytes + todays_tx_bytes
+	total_yest = yesterdays_rx_bytes + yesterdays_tx_bytes
+	email_message = "<p><u><strong>Network Usage:</strong></u></p>"
+	email_message += '<p>&nbsp; &nbsp; Total Network Traffic (Read + Write) for today is <span style="font-size:14px"><span style="color:#ff8c00"><strong>'\
+					 + str((total_today) / (1024 * 1024 * 1024)) + " GB</strong></span></span></p>"
+	if (total_today > total_yest):
+		email_message += "<p>&nbsp; &nbsp; This is <strong>" + str((total_today - total_yest)/ (1024 * 1024 * 1024))\
+						 + ' GB </strong><span style="color:#ff0000"><u><strong> more </strong></u></span> than yesterday</p>'
+	else:
+		email_message += "<p>&nbsp; &nbsp; This is <strong>" + str((total_yest - total_today)/ (1024 * 1024 * 1024))\
+						 + ' GB</strong> <span style="color:#008000"><u><strong>less</strong></u></span> than yesterday</p>'
+
+	return email_message
+
 
 def check_options(options):
 	if options.target_email == "":
@@ -153,6 +230,18 @@ if __name__ == '__main__':
 					  dest="target_email",
 					  help="email_id to send the summary email to")
 
+	parser.add_option("-s", "--email-server",
+					  dest="email_server",
+					  help="email server for sending email from")
+
+	parser.add_option("-U", "--email-server-login",
+					  dest="email_server_login",
+					  help="email server login for sending email")
+
+	parser.add_option("-P", "--email-server-password",
+					  dest="email_server_password",
+					  help="email server password for sending email from")
+
 	(options, args) = parser.parse_args()
 
 	if options.server is None or options.uid is None or options.password is None or options.target_email is None:
@@ -171,4 +260,4 @@ if __name__ == '__main__':
 		print "Unable to connect to vRNI"
 		sys.exit(1)
 
-	send_summary_email(session, "")
+	send_summary_email(session, options)
