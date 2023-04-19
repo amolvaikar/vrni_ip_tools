7892ba9a27039516b0069c731065a81231c724b2
avaikar <avaikar@vmware.com>
2023-04-20 01:14:32 +0200

    Script to create important changes pinboard

--- ChangesPinboardCreator/create_changes_pinboard.py
+++ ChangesPinboardCreator/create_changes_pinboard.py
@@ -35,8 +35,16 @@ def open_vrni_session(url, user_id, password):
 		# Instead of requests.get(), you'll use session.get()
 		response = session.post(url+"/api/ni/auth/token", data=data, verify=False, headers={'content-type':'application/json', 'accept':'application/json'})
 		#print response
+
+		if response.status_code != 200:
+			print("Failed to authenticate")
+			return
+
 		loaded_json = json.loads(response.content)
 		session.headers["Authorization"] = "NetworkInsight " + loaded_json["token"]
+		session.headers["Content-Type"] = "application/json"
+		session.headers["Accept"] = "application/json"
+		session.auth = None
 		return session
 	except requests.exceptions.ConnectionError as connection_exception:
 		print ("Failed to connect to " + url)
@@ -58,42 +66,41 @@ def create_changes_pinboard(session, pinboard_name = "Important Changes"):
 def generate_crud_pinboards_for_entity_type(session, pinboard_id, entity_type, create = True, update = True, delete = True):
 	if create:
 		# Add discovery pinboard:
-		discovery_pinboard_name = "New {1} Discovered".format(entity_type)
-		discovery_alert_name = "Entity type {1} discovered".format(entity_type)
-		discovery_query = 'entity.name of change alert where message = "{1}"'.format(discovery_alert_name)
+		discovery_pinboard_name = "New {} Discovered".format(entity_type)
+		discovery_alert_name = "Entity type {} discovered".format(entity_type)
+		discovery_query = 'entity.name of change alert where message = \\"{}\\"'.format(discovery_alert_name)
 		add_pin_to_pinboard(session, pinboard_id, discovery_pinboard_name, discovery_query)
 
 	if update:
 		# Add property update pinboard:
-		update_pinboard_name = "Changed {1}".format(entity_type)
-		update_alert_name = "Entity type {1} properties updated".format(entity_type)
-		update_query = 'change alert where message = "{1}" group by entity, changedProperties.propertyPath'.format(update_alert_name)
+		update_pinboard_name = "Changed {}".format(entity_type)
+		update_alert_name = "Entity type {} properties updated".format(entity_type)
+		update_query = 'change alert where message = \\"{}\\" group by entity, changedProperties.propertyPath'.format(update_alert_name)
 		add_pin_to_pinboard(session, pinboard_id, update_pinboard_name, update_query)
 
 	if delete:
 		# Add entity deleted pinboard:
-		deletion_pinboard_name = "Deleted {1}".format(entity_type)
-		deletion_alert_name = "Entity type {1} deleted".format(entity_type)
-		deletion_query = 'entity.name of change alert where message = "{1}"'.format(deletion_alert_name)
+		deletion_pinboard_name = "Deleted {}".format(entity_type)
+		deletion_alert_name = "Entity type {} deleted".format(entity_type)
+		deletion_query = 'entity.name of change alert where message = \\"{}\\"'.format(deletion_alert_name)
 		add_pin_to_pinboard(session, pinboard_id, deletion_pinboard_name, deletion_query)
+
 def add_pin_to_pinboard(session, pinboard_id, pin_name, pin_query):
-	body = '''{"name": "{1}", "query": "{2}"}'''.format(pin_name, pin_query)
-	response = session.post(url+"/api/ni/pinboards/{id}/pins".format(pinboard_id), data=body, verify=False)
+	body = '''{{"name": "{0}", "query": "{1}"}}'''.format(pin_name, pin_query)
+	response = session.post(url+"/api/ni/pinboards/{}/pins".format(pinboard_id), data=body, verify=False)
 	loaded_json = json.loads(response.content)
 	return
 
 # returns the pinboard id if succcessful, None in case of failures.
 def create_pinboard(session, pinboard_name, pinboard_description):
-	body = '''{"name": "{1}", "description": "{2}"}'''.format(pinboard_name, pinboard_description)
+	body = '''{{"name": "{0}", "description": "{1}"}}'''.format(pinboard_name, pinboard_description)
 	response = session.post(url+"/api/ni/pinboards", data=body, verify=False)
-	if response.status == 201:
+	if response.status_code == 201:
 		loaded_json = json.loads(response.content)
 		return loaded_json["id"]
+	print("Failed to create pinboard, please check if the pinboard already exists or if you have used admin/member login credentials")
 	return None
 def check_options(options):
-	if options.target_email == "":
-		print ("email id is mandatory")
-		return False
 	return True
 
 if __name__ == '__main__':
@@ -112,12 +119,12 @@ if __name__ == '__main__':
 
 	parser.add_option("-n", "--name",
 					  dest="pinboard_name",
-					  help="Name to be used for the important changes pinboard")
+					  help="[Optional] Name to be used for the important changes pinboard")
 
 
 	(options, args) = parser.parse_args()
 
-	if options.server is None or options.uid is None or options.password is None or options.pinboard_name is None:
+	if options.server is None or options.uid is None or options.password is None:
 		parser.print_help()
 		print ("Insufficient arguments")
 		sys.exit(1)
